d0b3c7e809fb807d91df671ab941edee51dc8619
ornob90 <oornob49@gmail.com>
2023-09-24 19:27:42 +0600

    project setup with required dependencies and setup all the folder

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import RootLayout from "../Layout/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+  },
+]);
+
+export default router;
